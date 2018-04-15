af426bacbb5ae2bb15293a6d0f3f0757a5afc131
Andriy <alavrinok@gmail.com>
2018-04-15 11:38:14 +0000

    Initial

--- js/top.js
+++ js/top.js
@@ -6,7 +6,7 @@ $(function() {
  $.fn.scrollToTop = function() {
   $(this).hide().removeAttr("href");
   if ($(window).scrollTop() >= "250") $(this).fadeIn("slow")
-  var scrollDiv = $(this);
+  let scrollDiv = $(this);
   $(window).scroll(function() {
    if ($(window).scrollTop() <= "250") $(scrollDiv).fadeOut("slow")
    else $(scrollDiv).fadeIn("slow")
